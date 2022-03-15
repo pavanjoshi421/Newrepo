ea6557dd4e2e265d38452ce3ae346987dd5752f8
Dell <Dell@DESKTOP-DFDQ44H>
2022-03-15 16:31:36 +0530

    New repo 1

---
+++ src/test/java/com/crm/demo/Demo2Test.java
@@ -0,0 +1,14 @@
+package com.crm.demo;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+
+	@Test
+	public void Demo()  {
+		System.out.println("work is there");
+		System.err.println("Login to tyss");
+		System.out.println('P');
+		System.out.println(421);
+	}
+}
