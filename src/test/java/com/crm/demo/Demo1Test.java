0fbbdce1da65b07c71f09d2b84b822ceb239d465
Dell <Dell@DESKTOP-DFDQ44H>
2022-03-15 16:29:14 +0530

    New repo 1

---
+++ src/test/java/com/crm/demo/Demo1Test.java
@@ -0,0 +1,21 @@
+package com.crm.demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void Demo()  {
+		System.out.println("User 1 Commit");
+		
+		System.out.println("User 2 Commit");
+		
+		System.out.println("Eat something if u hungrey");
+		System.out.println("Otherwise do work");
+		System.out.println("Or Else go hell");
+		System.out.println("Go heaven");
+		
+		
+		
+	}
+}
