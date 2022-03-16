8cf3efce5523b8ea6387179fb9998f598e92b55e
Dell <Dell@DESKTOP-DFDQ44H>
2022-03-16 10:47:42 +0530

    commit by pavan

---
+++ src/test/java/com/crm/demo/Demo3.java
@@ -0,0 +1,14 @@
+package com.crm.demo;
+
+import org.testng.annotations.Test;
+
+public class Demo3 {
+	@Test
+	public void demo()  {
+		String browser = System.getProperty("browser");
+		String uesrname = System.getProperty("username");
+		String password = System.getProperty("password");
+
+	}
+
+}
